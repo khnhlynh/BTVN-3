2ee561d867e7f6e9c04af57a6e9c866f4fa18d4b
tvqua <tvqua@LAPTOP-HLCS8RCT>
2023-08-26 18:19:06 +0700

    Add project files.

---
+++ BTVN  3/Controllers/TongHaiSoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Evaluation;
+
+namespace BTVN__3.Controllers
+{
+    public class TongHaiSoController : Controller
+    { 
+        public IActionResult Index()
+        {
+            ViewData["Message"] = " ";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int soNguyen1, int soNguyen2)
+        {
+            ViewData["SoNguyen1"] = soNguyen1;
+            ViewData["SoNguyen2"] = soNguyen2;
+            ViewData["KetQua"] = soNguyen1 + soNguyen2;
+            return View();
+        }
+    }
+}
